116027535a124d5d6cf05afeace4df94ac1fb09c
Jacob Shuman <jacob.shuman7@gmail.com>
2019-05-27 22:08:43 -0400

    refactor: uncommented PlexMediaType getters

--- lib/src/media_type/plex_media_type.dart
+++ lib/src/media_type/plex_media_type.dart
@@ -1,29 +1,21 @@
 import "package:dart_plex_api/dart_plex_api.dart";
 
 abstract class PlexMediaType {
-  // int id;
-  // String typeString;
-  // String title;
-  // String element;
-  // List<int> related;
-
-  // static const movie = PlexMovie();
-
-  // static PlexMediaType get movie => PlexMovie();
-  // static PlexMediaType get show => PlexShow();
-  // static PlexMediaType get season => PlexSeason();
-  // static PlexMediaType get episode => PlexEpisode();
-  // static PlexMediaType get trailer => PlexTrailer();
-  // static PlexMediaType get comic => PlexComic();
-  // static PlexMediaType get person => PlexPerson();
-  // static PlexMediaType get artist => PlexArtist();
-  // static PlexMediaType get album => PlexAlbum();
-  // static PlexMediaType get track => PlexTrack();
-  // static PlexMediaType get photoAlbum => PlexPhotoAlbum();
-  // static PlexMediaType get picture => PlexPicture();
-  // static PlexMediaType get photo => PlexPhoto();
-  // static PlexMediaType get clip => PlexClip();
-  // static PlexMediaType get playlistItem => PlexPlaylistItem();
+  static PlexMediaType get movie => PlexMovie();
+  static PlexMediaType get show => PlexShow();
+  static PlexMediaType get season => PlexSeason();
+  static PlexMediaType get episode => PlexEpisode();
+  static PlexMediaType get trailer => PlexTrailer();
+  static PlexMediaType get comic => PlexComic();
+  static PlexMediaType get person => PlexPerson();
+  static PlexMediaType get artist => PlexArtist();
+  static PlexMediaType get album => PlexAlbum();
+  static PlexMediaType get track => PlexTrack();
+  static PlexMediaType get photoAlbum => PlexPhotoAlbum();
+  static PlexMediaType get picture => PlexPicture();
+  static PlexMediaType get photo => PlexPhoto();
+  static PlexMediaType get clip => PlexClip();
+  static PlexMediaType get playlistItem => PlexPlaylistItem();
 
   static PlexMediaType fromTypeString(String type) {
     switch (type) {
