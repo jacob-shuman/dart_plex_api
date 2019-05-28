683684ba6d6ac07e404d811025dc0c79fbb45eb9
Jacob Shuman <jacob.shuman7@gmail.com>
2019-05-27 22:22:35 -0400

    refactor: PlexClient now extends PlexObject

--- lib/src/result/plex_client.dart
+++ lib/src/result/plex_client.dart
@@ -1,4 +1,9 @@
-class PlexClient {
+import "package:meta/meta.dart";
+import "package:dart_plex_api/dart_plex_api.dart";
+
+class PlexClient extends PlexObject {
+  static final String path = "clients";
+
   final String name;
   final String host;
   final String address;
@@ -12,6 +17,7 @@ class PlexClient {
   final String protocolCapabilities;
 
   PlexClient({
+    @required PlexConnection connection,
     this.name,
     this.host,
     this.address,
@@ -23,5 +29,42 @@ class PlexClient {
     this.deviceClass,
     this.protocolVersion,
     this.protocolCapabilities,
-  });
+  }) : super(
+          connection: connection,
+        );
+
+  @override
+  PlexClient.fromJson({
+    @required PlexConnection connection,
+    @required dynamic json,
+  })  : assert(json != null),
+        this.name = json["name"],
+        this.host = json["host"],
+        this.address = json["address"],
+        this.port = json["port"],
+        this.machineIdentifier = json["machineIdentifier"],
+        this.version = json["version"],
+        this.protocol = json["protocol"],
+        this.product = json["product"],
+        this.deviceClass = json["deviceClass"],
+        this.protocolVersion = json["protocolVersion"],
+        this.protocolCapabilities = json["protocolCapabilities"],
+        super(
+          connection: connection,
+        );
+
+  @override
+  Map<String, dynamic> toJson() => {
+        "name": this.name,
+        "host": this.host,
+        "address": this.address,
+        "port": this.port,
+        "machineIdentifier": this.machineIdentifier,
+        "version": this.version,
+        "protocol": this.protocol,
+        "product": this.product,
+        "deviceClass": this.deviceClass,
+        "protocolVersion": this.protocolVersion,
+        "protocolCapabilities": this.protocolCapabilities,
+      };
 }
