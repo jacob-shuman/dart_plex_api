b3d34082fba9ba89e53a319057d9d823587290c3
Jacob Shuman <jacob.shuman7@gmail.com>
2019-05-27 22:08:17 -0400

    chore: updated package exports

--- lib/dart_plex_api.dart
+++ lib/dart_plex_api.dart
@@ -1,30 +1,55 @@
 library dart_plex_api;
 
-// Classes
-export 'src/plex_client.dart';
+// Base
+export 'src/plex_connection.dart';
 export 'src/plex_credentials.dart';
 export 'src/plex_headers.dart';
 export 'src/plex_authorization.dart';
 
+// Object
+export 'src/result/plex_object.dart';
+export 'src/result/plex_raw_result.dart';
+export 'src/result/plex_root.dart';
+export 'src/result/plex_server.dart';
+export 'src/result/plex_client.dart';
+export 'src/result/plex_directory.dart';
+export 'src/result/plex_location.dart';
+
+// Library
+export 'src/result/library/plex_library.dart';
+export 'src/result/library/section/plex_library_section_index.dart';
+export 'src/result/library/section/plex_library_section.dart';
+export 'src/result/library/section/plex_artist_section.dart';
+
+// Route
+export "src/route/plex_route.dart";
+export "src/route/plex_raw_route.dart";
+export "src/route/plex_root_route.dart";
+export "src/route/plex_library_route.dart";
+export "src/route/plex_library_section_index_route.dart";
+export "src/route/plex_library_section_route.dart";
+export "src/route/plex_servers_route.dart";
+
 // Media Types
-export 'src/media_types/plex_media_type.dart';
-export 'src/media_types/plex_movie.dart';
-export 'src/media_types/plex_show.dart';
-export 'src/media_types/plex_season.dart';
-export 'src/media_types/plex_episode.dart';
-export 'src/media_types/plex_trailer.dart';
-export 'src/media_types/plex_comic.dart';
-export 'src/media_types/plex_person.dart';
-export 'src/media_types/plex_artist.dart';
-export 'src/media_types/plex_album.dart';
-export 'src/media_types/plex_track.dart';
-export 'src/media_types/plex_photo_album.dart';
-export 'src/media_types/plex_picture.dart';
-export 'src/media_types/plex_photo.dart';
-export 'src/media_types/plex_clip.dart';
-export 'src/media_types/plex_playlist_item.dart';
+export 'src/media_type/plex_media_type.dart';
+export 'src/media_type/plex_movie.dart';
+export 'src/media_type/plex_show.dart';
+export 'src/media_type/plex_season.dart';
+export 'src/media_type/plex_episode.dart';
+export 'src/media_type/plex_trailer.dart';
+export 'src/media_type/plex_comic.dart';
+export 'src/media_type/plex_person.dart';
+export 'src/media_type/plex_artist.dart';
+export 'src/media_type/plex_album.dart';
+export 'src/media_type/plex_track.dart';
+export 'src/media_type/plex_photo_album.dart';
+export 'src/media_type/plex_picture.dart';
+export 'src/media_type/plex_photo.dart';
+export 'src/media_type/plex_clip.dart';
+export 'src/media_type/plex_playlist_item.dart';
 
 // Exceptions
 export 'src/exceptions/invalid_credentials_exception.dart';
 export 'src/exceptions/missing_headers_exception.dart';
+export 'src/exceptions/unknown_media_type_exception.dart';
 export 'src/exceptions/unknown_exception.dart';
